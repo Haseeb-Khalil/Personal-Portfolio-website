8d6c972758d9ba89bb46c9fc2ea661ed64db1bb5
haseeb3927 <haseeb3927@gmail.com>
2022-03-02 14:59:12 +0000

    Header Section completed with styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/Haseeb.jpg";
+import HeaderSocials from "./HeaderSocials";
+import { BsArrowRightSquare } from "react-icons/bs";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Haseeb Khalil</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+          <BsArrowRightSquare className="arrow__down" />
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
