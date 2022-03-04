96eb061040b3ce7d06250187c5ce709058dab4f3
haseeb3927 <haseeb3927@gmail.com>
2022-03-04 17:25:07 +0000

    Footer Component completed and styled
    All components have been created. Needs some changes. Some refactoring.
    Also needs to add some real data

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
+import { FiTwitter } from "react-icons/fi";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        HASEEB
+      </a>
+
+      {/* Page links */}
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      {/* Social Profiles */}
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://instagram.com">
+          <BsInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <FiTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Haseeb's Personal Website. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
