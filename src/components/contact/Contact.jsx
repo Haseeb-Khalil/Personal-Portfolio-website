5bde7903293fd062ef7944b6997715b9da5fa7ba
haseeb3927 <haseeb3927@gmail.com>
2022-03-05 08:52:34 +0000

    Personal contact details SECURED using HTML entities
    r

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -35,7 +35,10 @@ const Contact = () => {
             <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>haseeb.khalil3927@gmail.com</h5>
-            <a href="mailto:haseeb.khalil3927@gmail.com" target="_blank">
+            <a
+              href="&#x6d;&#x61;&#x69;&#x6c;&#x74;&#x6f;&#x3a;&#x68;&#x61;&#x73;&#x65;&#x65;&#x62;&#x2e;&#x6b;&#x68;&#x61;&#x6c;&#x69;&#x6c;&#x33;&#x39;&#x32;&#x37;&#x40;&#x67;&#x6d;&#x61;&#x69;&#x6c;&#x2e;&#x63;&#x6f;&#x6d;"
+              target="_blank"
+            >
               Send Email
             </a>
           </article>
@@ -43,7 +46,10 @@ const Contact = () => {
             <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>Haseeb Khalil</h5>
-            <a href="https://m.me/haseeb.khalil.77" target="_blank">
+            <a
+              href="&#x68;&#x74;&#x74;&#x70;&#x73;&#x3a;&#x2f;&#x2f;&#x6d;&#x2e;&#x6d;&#x65;&#x2f;&#x68;&#x61;&#x73;&#x65;&#x65;&#x62;&#x2e;&#x6b;&#x68;&#x61;&#x6c;&#x69;&#x6c;&#x2e;&#x37;&#x37;"
+              target="_blank"
+            >
               Send Message
             </a>
           </article>
@@ -52,7 +58,7 @@ const Contact = () => {
             <h4>WhatsApp</h4>
             <h5>+44 123456789</h5>
             <a
-              href="https://api.whatsapp.com/send?phone=+447463646282"
+              href="&#x68;&#x74;&#x74;&#x70;&#x73;&#x3a;&#x2f;&#x2f;&#x61;&#x70;&#x69;&#x2e;&#x77;&#x68;&#x61;&#x74;&#x73;&#x61;&#x70;&#x70;&#x2e;&#x63;&#x6f;&#x6d;&#x2f;&#x73;&#x65;&#x6e;&#x64;&#x3f;&#x70;&#x68;&#x6f;&#x6e;&#x65;&#x3d;&#x2b;&#x34;&#x34;&#x37;&#x34;&#x36;&#x33;&#x36;&#x34;&#x36;&#x32;&#x38;&#x32;"
               target="_blank"
             >
               Send Message
